9e1ced730d13ae95b5dbde6ae1d8c1cd3a170d00
jamshidew <j79056394@gmail.com>
2025-04-24 21:33:04 +0500

    fixed

--- src/components/CartButton.jsx
+++ src/components/CartButton.jsx
@@ -2,10 +2,6 @@ import { useState, useEffect, useContext } from "react";
 import { CartContext } from "../context/CartContext.jsx";
 import { useState, useEffect } from "react";
 
-const CartButton = ({ product }) => {
-  const { cartDatas, setCartDatas } = useContext(CartContext);
-    const existing = cartDatas.find((item) => item.id === product.id);
-    const [quantity, setQuantity] = useState(existing?.quantity || 0);
 const CartButton = ({ product, cartDatas, setCartDatas }) => {
   const existing = cartDatas.find((item) => item.id === product.id);
   const [quantity, setQuantity] = useState(existing?.quantity || 0);
